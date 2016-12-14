94e58f46afc88a0ede08448c3b018af3c0efbfe6
Leonardo BALLAND <balland.leonardo@outlook.fr>
2016-12-14 20:10:21 +0000

    Pas à pas ProductsModel

--- app/Model/ProductsModel.php
+++ app/Model/ProductsModel.php
@@ -1,7 +1,11 @@
 // Créer une classe ProductsModel, qui hérite de Model, en spécifiant le namespace
 // Créer une méthode getProduct() qui prend en paramètre un id
 // créer dans cette méthode une variable $specifications qui est une instance de SpecificationsModel
-// utiliser la funtion search() pour récupérer toutes les specs qui contiennent l'id du produit dans la colonne product_id => exemple: search()
-// 
+// utiliser la funtion search() pour récupérer toutes les specs qui contiennent l'id du produit dans la colonne product_id => exemple: search(['colonne visée' => 'valeur à chercher'], 'operator AND ou OR');
+// stocker le résultat qui est un tableau dans une variable $specs
+// utiliser la methode find() en lui passant l'id du produit pour récupérer les infos du produit
+// stocker le résultat dans une variable $product
+// créer une nouvelle variable $res et stocker les deux tableaux dedans
+// retourner ensuite cette variable $res pour le controller
 
 <?php
