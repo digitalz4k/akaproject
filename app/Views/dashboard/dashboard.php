cc59b18e0a2e234a53cc853a5026d47fb0437716
clo <chloe_car@outlook.fr>
2016-12-22 12:32:47 +0000

    update dashboard

--- app/Views/dashboard/dashboard.php
+++ app/Views/dashboard/dashboard.php
@@ -12,16 +12,15 @@
 				<!--               ajouter des specs-->
 				<!--               specs basic  avec un  select multiple-->
 			    <!--               specs detail-->
-					    
+			    <li> <a href="#"> Modification / Supression produit</a></li>
 				<li> <a href="#"> Nouvel évènement</a></li>
-				<li> <a href="#"><i class="glyphicon glyphicon-cog" aria-hidden="true"></i> Paramètres</a></li>
 				<li> <a href="#"><i class="glyphicon glyphicon-off" aria-hidden="true"></i> Déconnexion</a></li>
 			</ul>
 		</nav>
-    </div>
+    
         <div id="dashboard-section">
             <div class="col-xs-8">
-                <div class="form-group">
+                <div class="form-group new-produit">
                     <label for="nom-produit">Nom du produit:</label>
                     <input type="text" class="form-control" id="nom-produits">
                 </div>
@@ -30,39 +29,38 @@
                     <textarea class="form-control" rows="5" id="comment"></textarea>
                 </div>
                 <div class="form-group">
-                    <label for="sel1">Spéc:</label>
-                    <select class="form-control" id="sel1">
-                        <option></option>
-                        <option></option>
-                        <option></option>
-                        <option></option>
-                    </select>
+                    <label for="sel1">Spécifications:</label>
+            <!--*********************CREATION DE SPEC*******************************************        -->
+                <div>
+                    <button type="button" class="btn btn-primary btn-create-spec">Create spec</button>
+                    <!--quand click sur bouton btn-create-spec form d'ajout de spec s'affiche-->
+                <div class="form-spec">
+                <div class="form-group new-spec">
+                    <label for="nom-produit">Nom de la spéc:</label>
+                    <input type="text" class="form-control" id="nom-produits">
                 </div>
-                
-            </div>
-            
-        </div>
+                <div class="form-group">
+                    <label for="description-produit">Description de la spec:</label>
+                    <textarea class="form-control" rows="5" id="comment"></textarea>
+                    <!--Ajouter soit dans spec basic, soit dans détail-->
+                <div class="btn-group" data-toggle="buttons">
+                    <label class="btn btn-primary active">
+                        <input type="radio" name="options" id="spec-basic" autocomplete="off" checked> Spec basic
+                    </label>
+                    <label class="btn btn-primary">
+                        <input type="radio" name="options" id="spec-detail" autocomplete="off"> Spec detail
+                    </label>
+                <div>
+                    <button type="button" class="btn btn-primary btn-create-spec">Ajouter</button>
+                </div> 
+                </div>
+        </div>        
     </div>
-    
 </div>
-
-
-<!--section gauche nav -->
-
-<!--ajouter un nouveau produit-->
-<!--            form -->
-<!--        nouvel évenement-->
-        
-<!--section droite content-->
-<!--    form ajout -->
-<!--        nom du produit-->
-<!--        description-->
-<!--        ajouter des spec -->
-<!--            avec menu qui deroule avec bouton création de spec -->
-<!--                spec basic  select multiple -->
-<!--                spec detail -->
-
 <?php $this->stop('main_content') ?>
+<?php $this->start('js') ?>
+	<script src="<?= $this->assetUrl('js/dashboard.js') ?>"></script>
+<?php $this->stop('js') ?>
 
 
 
