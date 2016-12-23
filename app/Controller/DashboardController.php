45eff6aea1434fd3fdaa84463d65a5432e26fa9c
Leonardo BALLAND <balland.leonardo@outlook.fr>
2016-12-23 21:18:54 +0000

    Cleaning directories

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -4,20 +4,7 @@ namespace Controller;
 class DashboardController extends \W\Controller\Controller
 {
     public function dashboard () {
-        if(!empty($_FILES["picture"]["name"]))
-        {
-            $upload = new \Controller\UploadSingleton\UploadController("uploads/", ['png', 'gif', 'jpg', 'jpeg']);
-            
-            $files = $upload->uploadThis($_FILES);
-            $json = $upload->getJSON();
-            
-            debug($files);
-            debug($json);
-
-        } else {
-            $this->show ("dashboard/dashboard");   
-        }
-        
+        $this->show ("dashboard/dashboard");
     }
     
     public function login(){
