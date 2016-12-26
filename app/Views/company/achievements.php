253dd4d4b4a051487e67ab132d74c680b1b87c81
Lionel <lionel.lft@me.com>
2016-12-26 16:19:37 +0000

    add style and content on achievements page

--- app/Views/company/achievements.php
+++ app/Views/company/achievements.php
@@ -12,65 +12,122 @@
                 <div class="col-xs-3 ach-tab-menu">
                   <div class="list-group">
                     <a href="#" class="list-group-item active text-center">
-                      <h4></h4><br/>Solar telescope
+                      <h4>Solar telescope</h4>
                     </a>
                     <a href="#" class="list-group-item text-center">
-                      <h4></h4><br/>PW lasers
+                      <h4>PW lasers</h4>
                     </a>
                     <a href="#" class="list-group-item text-center">
-                      <h4></h4><br/>Exemple 3
+                      <h4>Exemple 3</h4>
                     </a>
                     <a href="#" class="list-group-item text-center">
-                      <h4></h4><br/>Exemple 4
+                      <h4>Exemple 4</h4>
                     </a>
                     <a href="#" class="list-group-item text-center">
-                      <h4></h4><br/>Exemple 5
+                      <h4>Exemple 5</h4>
                     </a>
                   </div>
                 </div>
                 <div class="col-xs-9 ach-tab">
                     <!-- First section -->
                     <div class="ach-tab-content active">
-                        <div class="center">
-                          <h1>Solar télescope</h1>
-                          <h2>Cooming Soon</h2>
-                          <h3>Solar telescope</h3>
+                        <div class="center col-xs-11 col-xs-offset-1">
+                          <h3>Solar télescope</h3>
+                          <h5>Cooming Soon</h5>
+                          <img src="<?= $this->assetUrl('img/product.jpg') ?>" class="image responsive"/>
                         </div>
                     </div>
                     <!-- Second section -->
                     <div class="ach-tab-content">
-                        <center>
-                          <h1></h1>
-                          <h2>Cooming Soon</h2>
+                        <div class="center col-xs-11 col-xs-offset-1">
                           <h3>PW lasers</h3>
-                        </center>
+                          <h5>Cooming Soon</h5>
+                          <h6>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                          sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                          magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                          quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+                          ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
+                          dolor in hendrerit in vulputate velit esse molestie consequat,
+                          vel illum dolore eu feugiat nulla facilisis at vero eros et
+                          accumsan et iusto odio dignissim qui blandit praesent luptatum
+                          zzril delenit augue duis dolore te feugait nulla facilisi.
+                          Nam liber tempor cum soluta nobis eleifend option congue
+                          nihil imperdiet doming id quod mazim placerat facer possim
+                          assum. Typi non habent claritatem insitam; est usus legentis
+                          in iis qui facit eorum claritatem. Investigationes
+                          demonstraverunt lectores legere me lius quod ii legunt saepius.
+                          Claritas est etiam processus dynamicus, qui sequitur mutationem
+                          consuetudium lectorum. Mirum est notare quam littera gothica,
+                          quam nunc putamus parum claram, anteposuerit litterarum formas
+                          humanitatis per seacula quarta decima et quinta decima. Eodem
+                          modo typi, qui nunc nobis videntur parum clari, fiant sollemnes
+                          in futurum.</h6>
+                        </div>
                     </div>
         
                     <!-- Third section -->
                     <div class="ach-tab-content">
-                        <center>
-                          <h1></h1>
-                          <h2>Cooming Soon</h2>
+                        <div class="center col-xs-11 col-xs-offset-1">
                           <h3>Exemple 3</h3>
-                        </center>
+                          <h5>Cooming Soon</h5>
+                          <h6>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                          sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                          magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                          quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+                          ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
+                          dolor in hendrerit in vulputate velit esse molestie consequat,
+                          vel illum dolore eu feugiat nulla facilisis at vero eros et
+                          accumsan et iusto odio dignissim qui blandit praesent luptatum
+                          zzril delenit augue duis dolore te feugait nulla facilisi.
+                          Nam liber tempor cum soluta nobis eleifend option congue
+                          nihil imperdiet doming id quod mazim placerat facer possim
+                          assum. Typi non habent claritatem insitam; est usus legentis
+                          in iis qui facit eorum claritatem. Investigationes
+                          demonstraverunt lectores legere me lius quod ii legunt saepius.
+                          Claritas est etiam processus dynamicus, qui sequitur mutationem
+                          consuetudium lectorum. Mirum est notare quam littera gothica,
+                          quam nunc putamus parum claram, anteposuerit litterarum formas
+                          humanitatis per seacula quarta decima et quinta decima. Eodem
+                          modo typi, qui nunc nobis videntur parum clari, fiant sollemnes
+                          in futurum.</h6>
+                        </div>
                     </div>
 
                     <!-- Fourth section -->
                     <div class="ach-tab-content">
-                        <center>
-                          <h1></h1>
-                          <h2>Cooming Soon</h2>
+                        <div class="center col-xs-11 col-xs-offset-1">
                           <h3>Exemple 4</h3>
-                        </center>
+                          <h5>Cooming Soon</h5>
+                          <h6>lorem</h6>
+                        </div>
                     </div>
 
                     <!-- Fifth section -->
                     <div class="ach-tab-content">
-                        <center>
-                          <h1></h1>
-                          <h2>Cooming Soon</h2>
+                        <div class="center col-xs-11 col-xs-offset-1">
                           <h3>Exemple 5</h3>
-                        </center>
+                          <h5>Cooming Soon</h5>
+                          <h6>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                          sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                          magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                          quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+                          ut aliquip ex ea commodo consequat. Duis autem vel eum iriure
+                          dolor in hendrerit in vulputate velit esse molestie consequat,
+                          vel illum dolore eu feugiat nulla facilisis at vero eros et
+                          accumsan et iusto odio dignissim qui blandit praesent luptatum
+                          zzril delenit augue duis dolore te feugait nulla facilisi.
+                          Nam liber tempor cum soluta nobis eleifend option congue
+                          nihil imperdiet doming id quod mazim placerat facer possim
+                          assum. Typi non habent claritatem insitam; est usus legentis
+                          in iis qui facit eorum claritatem. Investigationes
+                          demonstraverunt lectores legere me lius quod ii legunt saepius.
+                          Claritas est etiam processus dynamicus, qui sequitur mutationem
+                          consuetudium lectorum. Mirum est notare quam littera gothica,
+                          quam nunc putamus parum claram, anteposuerit litterarum formas
+                          humanitatis per seacula quarta decima et quinta decima. Eodem
+                          modo typi, qui nunc nobis videntur parum clari, fiant sollemnes
+                          in futurum.</h6>
+                        </div>
                     </div>
                 </div>
           </div>
