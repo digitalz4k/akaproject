0a5042011e4523a8d4bb96e24649a2117ccd6d3d
Leonardo BALLAND <balland.leonardo@outlook.fr>
2016-12-27 16:29:40 +0000

    Product image updated

--- app/Views/products/products.php
+++ app/Views/products/products.php
@@ -16,7 +16,9 @@
             <?php foreach ($products as $key => $product)
             { ?>
             <li class="swipe-item col col-xs-4 <?php if($key == 1) { echo "active"; } ?>">
+                <?php if (isset($product["picture_url"])) { ?>
                 <img class="swipe-img" src="<?= $this->assetUrl('img/'.$product["picture_url"]) ?>" />
+                <?php } ?>
                 <div class="swipe-content">
                     <h3><?php echo $product['name']; ?></h3>
                     <p><?php echo $product['accroche']; ?></p>
