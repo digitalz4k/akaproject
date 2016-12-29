28cab0105357018bcfb398a130d0dff3b00dca5e
Lionel <lionel.lft@me.com>
2016-12-29 14:09:51 +0000

    added dynamic data on contact page

--- app/Views/contact/contact.php
+++ app/Views/contact/contact.php
@@ -20,20 +20,24 @@
                 <iframe class="img-responsive" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1450.787266043741!2d5.430373158298565!3d43.34409149478334!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9bfd1e3c17035%3A0x4ff62b83cfad4e04!2s2+Rue+Marc+Donadille%2C+13013+Marseille!5e0!3m2!1sfr!2sfr!4v1481811953486" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
         
+        <?php 
+            foreach ($datas as $data) {
+        ?>
+        
         <div class="col-xs-12 col-sm-12 col-md-6 panel panel-default">
             <div class="col-sm-4 col-md-12">
                 <h3><i class="fa fa-info"></i> Contact information</h3>
                 <ul class="list-unstyled">
-                    <li><a href="mailto:contact@akaoptics.com">contact@akaoptics.com</a></li>
-                    <li>Tel: +33 4 91 05 50 86</li>
-                    <li>Fax: +33 4 91 05 50 87</li>
+                    <li><a href="mailto:"<?= $data['website_contact_email'] ?>"><?= $data['website_contact_email'] ?></a></li>
+                    <li>Tel: <?= $data['website_contact_phone'] ?> </li>
+                    <li>Fax: <?= $data['website_contact_fax'] ?></li>
                 </ul>
             </div>
             <div class="col-sm-4 col-md-12">
                 <h3><i class="fa fa-location-arrow"></i> Address</h3>
                 <ul class="list-unstyled">
-                    <li>2 rue Marc Donadille</li>
-                    <li>13013 MARSEILLE</li>
+                    <li><?= $data['website_contact_street'] ?></li>
+                    <li><?= $data['website_contact_zip'] ?> <?= $data['website_contact_city'] ?></li>
                     <li>FRANCE</li>
                 </ul>
             </div>
@@ -43,12 +47,24 @@
                     <li>AKA OPTICS is a French SAS</li>
                     <li>(Société par Actions Simplifiée)</li>
                     <li>Capital: 100 000 €</li>
-                    <li>SIREN: 790 049 985 RCS Marseille</li>
+                    <li>SIREN: <?= $data['website_company_siren'] ?></li>
                 </ul>
             </div>
         </div>
+        
+        <?php
+            }
+        ?>
+        
     </div>
 </div>
+
+<?php 
+foreach ($data as $s_data) {
+}
+?>
+
+
 <?php $this->stop('main_content') ?>
 
 <?php $this->start('css') ?>
