8f55b0cc0dacdd7126d4728d66146e2aa1a3d5a2
clo <chloe_car@outlook.fr>
2017-01-04 11:35:23 +0000

    link social

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -30,9 +30,10 @@
 			<small>© AKA Optics SAS 2012-2016 | All Rights Reserved – Please use the contact information to report any problem with this web site.</small>
 		</p>
 		<ul class="social-icons">
-			<li><i class="fa fa-facebook" aria-hidden="true"></i></li>
-			<li><i class="fa fa-twitter" aria-hidden="true"></i></li>
-			<li><i class="fa fa-google-plus" aria-hidden="true"></i></li>
+			<li><a href="https://www.facebook.com/" target="blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+			<li><a href="https://twitter.com/" target="blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+			<li><a href="https://plus.google.com/" target="blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+			<li><a href="https://fr.linkedin.com/" target="blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
 		</ul>
 		
 
